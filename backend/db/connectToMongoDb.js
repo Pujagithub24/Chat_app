6c8875164ff805596cf06f402090b5683aaa842e
Pujagithub24 <135828788+Pujagithub24@users.noreply.github.com>
2024-07-27 13:39:07 +0530

    first commit

---
+++ backend/db/connectToMongoDb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectToMongoDb = async () => {
+    try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("connected to MongoDB");       
+    }
+
+     catch (error) {
+        console.log("Error connecting to MongoDb",error.message);
+    }
+}
+
+export default connectToMongoDb;
