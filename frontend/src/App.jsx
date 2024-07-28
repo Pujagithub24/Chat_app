c64f42b3af31637b6d018c222764ce45041e86d8
Pujagithub24 <135828788+Pujagithub24@users.noreply.github.com>
2024-07-28 17:24:13 +0530

    hooks and authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
-import Login from './pages/login/Login.jsx';
-import SignUp from './pages/signup/SignUp.jsx';
-import Home from './pages/home/Home.jsx';
+import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
+import Home from './pages/home/Home';
+import {Navigate, Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { useAuthContext } from './context/AuthContext';
 
 function App() {
 
+  const {authUser} = useAuthContext();
+
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-    <Home />
+     <Routes>
+      
+       <Route path ='/' element={authUser ? <Home /> : <Navigate to = {"/login"}/>} /> {/*here in case
+       of logout authuser is null due to which we will navigate to the login page */}
+       <Route path ='/login' element={authUser ? <Navigate to ='/' /> : <Login />} />
+       <Route path ='/signup' element={authUser ? <Navigate to ='/' /> : <SignUp />} />  {/* when user 
+       is authenticated go to home page if not authenticated then go to signup page */}
+    
+     </Routes>
+     <Toaster />
     </div>
   );
 }
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
   const Login = () => {
 	return (
@@ -29,9 +30,9 @@ import React from 'react'
 						/>
 					</div>
 
-					<a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
 						{"Don't"} have an account?
-					</a>
+					</Link>
 
 					<div>
 						<button className='btn btn-block btn-sm mt-2'>Login</button>
@@ -43,5 +44,52 @@ import React from 'react'
 	);
 };
 export default Login;
+
+//earlier
+// import React from 'react'
+
+//   const Login = () => {
+// 	return (
+// 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+// 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+// 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+// 					Login
+// 					<span className='text-blue-500'> ChatApp</span>
+// 				</h1>
+
+// 				<form>
+          
+// 					<div>
+// 						<label className='label p-2'>
+// 							<span className='text-base label-text'>Username</span>
+// 						</label>
+// 						<input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+// 					</div>
+
+// 					<div>
+// 						<label className='label'>
+// 							<span className='text-base label-text'>Password</span>
+// 						</label>
+// 						<input
+// 							type='password'
+// 							placeholder='Enter Password'
+// 							className='w-full input input-bordered h-10'
+// 						/>
+// 					</div>
+
+// 					<a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+// 						{"Don't"} have an account?
+// 					</a>
+
+// 					<div>
+// 						<button className='btn btn-block btn-sm mt-2'>Login</button>
+// 					</div>
+
+// 				</form>
+// 			</div>
+// 		</div>
+// 	);
+// };
+// export default Login;
 
 
